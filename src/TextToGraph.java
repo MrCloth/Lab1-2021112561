185ddb39c52c5d0e66732a3dbdda0f656e5ceb7d
cloth911 <2021112561@stu.hit.edu.cn>
2024-05-26 23:54:20 +0800

    Changes

--- src/TextToGraph.java
+++ src/TextToGraph.java
@@ -1,7 +1,7 @@
 //b2 change
 import java.io.*;
 import java.util.*;
-//the IDE GIT TEST
+//the IDE GIT TEST2
 public class TextToGraph {    public static void main(String[] args) {
     String filePath;
     filePath = "./examples";
